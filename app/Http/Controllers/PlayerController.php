a7140fa0b7c5b655e584b9e39c4a4fa16296f156
lard0503 <larodriguezc@uamv.edu.ni>
2024-09-04 18:49:00 -0600

    api show and store

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -15,4 +15,28 @@ class PlayerController extends Controller
         $players = Player::select('first_name as Nombre', 'last_name as Apellido', 'team as Equipo', 'position as Posicion', 'number as Dorsal')->orderBy('team')->orderBy('number') ->get();
         return response()->json(['status' => 'success', 'data' => $players]);   
     }
+
+    public function store(Request $request)
+    {
+        //
+        try {
+            $player = Player::create ($request->all());
+            return response()->json(['status' => 'success', 'message' => 'Jugador creado exitosamente', 'data' => $player]);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function show(string $id)
+    {
+        //
+        try{
+            $player = Player::findOrfail($id);
+            return response()->json(['status' => 'success', 'data' => $player]);
+
+        }catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
 }
--- app/Models/Player.php
+++ app/Models/Player.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Player extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['player_id','first_name', 'last_name','birth_date', 'team', 'position', 'number'];
 }
